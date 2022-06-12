ce7615f38348fed57d74bf7f953e56491f21cc93
Layanne Gomes de Moraes <layanne.moraes>
2022-06-12 10:57:01 -0300

    Atualização

--- Semana1Dia2/Dia2.cs
+++ Semana1Dia2/Dia2.cs
@@ -10,7 +10,7 @@ namespace Semana1Dia2
     {
         static void Main(string[] args)
         {
-            //ex01idade();
+            //ex01Idade();
             //ex02SomaDeNumeros();
             //ex03LetraMusica();
             //ex04Frase();
@@ -35,19 +35,18 @@ namespace Semana1Dia2
 
             Console.WriteLine("Digite 10 números para somar:");
 
-            num1 = int.Parse(Console.ReadLine()); 
+            num1 = int.Parse(Console.ReadLine());
             num2 = int.Parse(Console.ReadLine());
             num3 = int.Parse(Console.ReadLine());
-            num4 = int.Parse(Console.ReadLine());   
-            num5 = int.Parse(Console.ReadLine());   
+            num4 = int.Parse(Console.ReadLine());
+            num5 = int.Parse(Console.ReadLine());
             num6 = int.Parse(Console.ReadLine());
             num7 = int.Parse(Console.ReadLine());
-            num8 = int.Parse(Console.ReadLine());   
+            num8 = int.Parse(Console.ReadLine());
             num9 = int.Parse(Console.ReadLine());
             num10 = int.Parse(Console.ReadLine());
 
             Console.WriteLine($"A soma dos seus números é: {num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10}");
         }
-        static void
-    }
+    }  
 }
